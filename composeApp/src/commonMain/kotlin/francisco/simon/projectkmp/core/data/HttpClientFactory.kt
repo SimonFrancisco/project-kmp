ff810ce945e6abf49b3abeec63d8792e20a6e144
Simon Francisco <simonfrancisco69@outlook.com>
2026-03-24 21:19:18 +0700

    Refactor koin data module

--- composeApp/src/commonMain/kotlin/francisco/simon/projectkmp/core/data/HttpClientFactory.kt
+++ composeApp/src/commonMain/kotlin/francisco/simon/projectkmp/core/data/HttpClientFactory.kt
@@ -1,5 +1,82 @@
 package francisco.simon.projectkmp.core.data
 
+import francisco.simon.projectkmp.features.auth.domain.repository.AuthRepository
+import francisco.simon.projectkmp.features.auth.prefs.TokenStorage
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
+import io.ktor.client.plugins.cache.HttpCache
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.SIMPLE
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
 object HttpClientFactory {
-//    fun create(engine:HttpClie)
+    fun create(engine: HttpClientEngine): HttpClient {
+        return HttpClient(engine = engine) {
+            install(plugin = HttpCache)
+            install(plugin = ContentNegotiation) {
+                json(
+                    json = Json {
+                        ignoreUnknownKeys = true
+                        prettyPrint = true
+                        isLenient = true
+                    }
+                )
+            }
+            install(plugin = Logging) {
+                logger = Logger.SIMPLE
+                level = LogLevel.ALL
+            }
+        }
+    }
+
+    fun create(
+        engine: HttpClientEngine,
+        authRepository: AuthRepository,
+        tokenStorage: TokenStorage
+    ): HttpClient {
+        return HttpClient(engine = engine) {
+            install(plugin = HttpCache)
+            install(plugin = ContentNegotiation) {
+                json(
+                    json = Json {
+                        ignoreUnknownKeys = true
+                        prettyPrint = true
+                        isLenient = true
+                    }
+                )
+            }
+            install(plugin = Auth) {
+                bearer {
+                    loadTokens {
+                        tokenStorage.getTokens()
+                    }
+                    refreshTokens {
+                        val refreshToken = oldTokens?.refreshToken ?: return@refreshTokens null
+                        val newToken = authRepository.refreshToken(refreshToken).getOrNull()
+                        if (newToken != null) {
+                            tokenStorage.saveTokens(newToken)
+                            BearerTokens(
+                                newToken.accessToken,
+                                newToken.refreshToken
+                            )
+                        } else {
+                            tokenStorage.clear()
+                            null
+                        }
+                    }
+                }
+            }
+            install(plugin = Logging) {
+                logger = Logger.SIMPLE
+                level = LogLevel.ALL
+            }
+        }
+    }
 }
--- composeApp/src/commonMain/kotlin/francisco/simon/projectkmp/di/DataModule.kt
+++ composeApp/src/commonMain/kotlin/francisco/simon/projectkmp/di/DataModule.kt
@@ -1,5 +1,6 @@
 package francisco.simon.projectkmp.di
 
+import francisco.simon.projectkmp.core.data.HttpClientFactory
 import francisco.simon.projectkmp.core.data.impl.CoreRepositoryImpl
 import francisco.simon.projectkmp.core.domain.repository.CoreRepository
 import francisco.simon.projectkmp.features.auth.data.AuthRepositoryImpl
@@ -10,82 +11,21 @@ import francisco.simon.projectkmp.features.catalog.data.CatalogRepositoryImpl
 import francisco.simon.projectkmp.features.catalog.domain.repository.CatalogRepository
 import francisco.simon.projectkmp.features.search.data.SearchRepositoryImpl
 import francisco.simon.projectkmp.features.search.domain.repository.SearchRepository
-import io.ktor.client.HttpClient
-import io.ktor.client.plugins.auth.Auth
-import io.ktor.client.plugins.auth.providers.BearerTokens
-import io.ktor.client.plugins.auth.providers.bearer
-import io.ktor.client.plugins.cache.HttpCache
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logger
-import io.ktor.client.plugins.logging.Logging
-import io.ktor.client.plugins.logging.SIMPLE
-import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.json.Json
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 internal val dataModule = module {
     single(createdAtStart = true, qualifier = PUBLIC_CLIENT) {
-        HttpClient(engine = get()) {
-            install(plugin = HttpCache)
-            install(plugin = ContentNegotiation) {
-                json(
-                    json = Json {
-                        ignoreUnknownKeys = true
-                        prettyPrint = true
-                        isLenient = true
-                    }
-                )
-            }
-            install(plugin = Logging) {
-                logger = Logger.SIMPLE
-                level = LogLevel.ALL
-            }
-        }
+        HttpClientFactory.create(engine = get())
     }
 
     single(createdAtStart = true, qualifier = AUTH_CLIENT) {
-        HttpClient(engine = get()) {
-            install(plugin = HttpCache)
-            install(plugin = ContentNegotiation) {
-                json(
-                    json = Json {
-                        ignoreUnknownKeys = true
-                        prettyPrint = true
-                        isLenient = true
-                    }
-                )
-            }
-            install(Auth) {
-                bearer {
-                    val tokenStorage: TokenStorage = get()
-                    val authRepository: AuthRepository = get()
-                    loadTokens {
-                        tokenStorage.getTokens()
-                    }
-                    refreshTokens {
-                        val refreshToken = oldTokens?.refreshToken ?: return@refreshTokens null
-                        val newToken = authRepository.refreshToken(refreshToken).getOrNull()
-                        if (newToken != null) {
-                            tokenStorage.saveTokens(newToken)
-                            BearerTokens(
-                                newToken.accessToken,
-                                newToken.refreshToken
-                            )
-                        } else {
-                            tokenStorage.clear()
-                            null
-                        }
-                    }
-                }
-            }
-            install(plugin = Logging) {
-                logger = Logger.SIMPLE
-                level = LogLevel.ALL
-            }
-        }
+        HttpClientFactory.create(
+            engine = get(),
+            authRepository = get(),
+            tokenStorage = get()
+        )
     }
     single<AuthRepository> {
         AuthRepositoryImpl(
